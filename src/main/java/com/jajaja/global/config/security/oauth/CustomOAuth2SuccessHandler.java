357be3ef24a0bfdf541fda12b95c67d7246e4b76
Jinsoo Yoon <floreo1242@gmail.com>
2025-07-11 13:19:12 +0900

    ✨ feat: JWT 발급 및 전달을 위한 OAuth2 성공 핸들러 추가

--- src/main/java/com/jajaja/global/config/security/oauth/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/jajaja/global/config/security/oauth/CustomOAuth2SuccessHandler.java
@@ -1,9 +1,13 @@
 package com.jajaja.global.config.security.oauth;
 
+import com.jajaja.global.config.security.JwtProperties;
+import com.jajaja.global.config.security.JwtProvider;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -14,8 +18,40 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
+    private final JwtProvider jwtProvider;
+    private final JwtProperties jwtProperties;
+
+    @Value("${auth.redirect-url}")
+    private String redirectUrl;
+
+    @Value("${auth.cookie.domain}")
+    private  String cookieDomain;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        // TODO: OAuth2 인증 성공 시 처리 로직 구현
+        String accessToken = jwtProvider.generateAccessToken(authentication);
+        String refreshToken = jwtProvider.generateRefreshToken(authentication);
+        writeTokenCookies(response, accessToken, refreshToken);
+        response.sendRedirect(redirectUrl);
+    }
+
+    private void writeTokenCookies(HttpServletResponse response, String accessToken, String refreshToken) {
+        ResponseCookie accessTokenCookie = ResponseCookie.from("accessToken", accessToken)
+                .path("/")
+                .httpOnly(true)
+                .maxAge(jwtProperties.getExpiration().getAccess())
+                .domain(cookieDomain)
+                .build();
+
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", refreshToken)
+                .path("/")
+                .httpOnly(true)
+                .maxAge(jwtProperties.getExpiration().getRefresh())
+                .domain(cookieDomain)
+                .build();
+
+        response.addHeader("Set-Cookie", accessTokenCookie.toString());
+        response.addHeader("Set-Cookie", refreshTokenCookie.toString());
+
     }
 }
