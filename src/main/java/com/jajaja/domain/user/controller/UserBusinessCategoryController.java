00a25a92445f5d22d9a85e54952f9c7896c9fffe
YYJ <yoon20220823@dukcung.ac.kr>
2025-07-12 19:29:28 +0900

    ♻️ Refactor: Operation 작성, 사용하지 않는 Setter 삭제

--- src/main/java/com/jajaja/domain/user/controller/UserBusinessCategoryController.java
+++ src/main/java/com/jajaja/domain/user/controller/UserBusinessCategoryController.java
@@ -4,6 +4,7 @@ import com.jajaja.domain.user.dto.UserBusinessCategoryRequestDto;
 import com.jajaja.domain.user.service.UserBusinessCategoryCommandService;
 import com.jajaja.global.apiPayload.ApiResponse;
 import com.jajaja.global.config.security.annotation.Auth;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +23,10 @@ public class UserBusinessCategoryController {
      * @param dto 업종 등록 요청 DTO
      * @return 200 OK
      */
+    @Operation(
+            summary = "유저 업종 등록 API | by 구름/윤윤지",
+            description = "온보딩 단계에서 유저가 선택한 업종을 등록합니다."
+    )
     @PostMapping("/")
     public ApiResponse<Void> registerUserBusinessCategory(
             @Auth Long userId,
