f1749a6eacca8df4f0ddcbc95b05915873adccec
tl1l1l1s <s2oo1og24@gmail.com>
2025-07-12 21:15:40 +0900

    ✨ feat: 장바구니 아이템 삭제 API 구현

--- src/main/java/com/jajaja/domain/cart/service/CartCommandService.java
+++ src/main/java/com/jajaja/domain/cart/service/CartCommandService.java
@@ -1,6 +1,9 @@
 package com.jajaja.domain.cart.service;
 
 import com.jajaja.domain.cart.dto.CartProductAddRequestDto;
+import com.jajaja.domain.cart.dto.CartProductDeleteRequestDto;
+
 public interface CartCommandService {
 	public void addOrUpdateCartProduct(Long memberId, CartProductAddRequestDto request);
+	public void deleteCartProduct(Long memberId, CartProductDeleteRequestDto request);
 }
--- src/main/java/com/jajaja/domain/cart/service/CartCommandServiceImpl.java
+++ src/main/java/com/jajaja/domain/cart/service/CartCommandServiceImpl.java
@@ -1,6 +1,7 @@
 package com.jajaja.domain.cart.service;
 
 import com.jajaja.domain.cart.dto.CartProductAddRequestDto;
+import com.jajaja.domain.cart.dto.CartProductDeleteRequestDto;
 import com.jajaja.domain.cart.entity.Cart;
 import com.jajaja.domain.cart.entity.CartProduct;
 import com.jajaja.domain.cart.repository.CartProductRepository;
@@ -32,33 +33,58 @@ public class CartCommandServiceImpl implements CartCommandService {
 	public void addOrUpdateCartProduct(Long memberId, CartProductAddRequestDto request) {
 		log.info("[CartCommandService] 사용자 {}의 장바구니에 아이템 {} 추가/수정", memberId, request.productId());
 		
+		CartOpterationContext context = prepareCartOperationContext(memberId, request.productId(), request.optionId());
+		
+		Optional<CartProduct> existingItem = cartProductRepository.findByCartIdAndProductId(context.cart().getId(), context.product().getId());
+		
+		existingItem.ifPresentOrElse(
+				item -> {
+					log.info("[CartCommandService] 기존 아이템 {}의 옵션과 수량을 변경합니다.", item.getId());
+					item.update(context.productOption(), request.quantity());
+				},
+				() -> {
+					log.info("[CartCommandService] 장바구니에 새로 아이템 {}를 추가합니다.", request.productId());
+					CartProduct newCartProduct = CartProduct.create(context.cart(), context.product(), context.productOption(), request.quantity());
+					context.cart().addCartProduct(newCartProduct);
+					cartProductRepository.save( newCartProduct);
+				}
+		);
+	}
+	
+	@Override
+	public void deleteCartProduct(Long memberId, CartProductDeleteRequestDto request) {
+		log.info("[CartCommandService] 사용자 {}의 장바구니에 아이템 {} 삭제", memberId, request.productId());
+		
+		CartOpterationContext context = prepareCartOperationContext(memberId, request.productId(), request.optionId());
+		
+		CartProduct existingItem = cartProductRepository.findByCartIdAndProductId(context.cart().getId(), context.product().getId())
+				.orElseThrow(() -> new CartHandler((ErrorStatus.CART_PRODUCT_NOT_FOUND)));
+		cartProductRepository.delete(existingItem);
+	}
+	
+	/**
+	*  장바구니 내 아이템 명령 실행에 필요한 도메인 객체를 불러오는 과정입니다.
+	 *  option null 처리 등 중복되는 로직 떄문에 생성하였습니다.
+	 *
+	 * @return 	Cart, Product, ProductOption을 포함한 CartOpterationContext
+	* */
+	private CartOpterationContext prepareCartOperationContext(Long memberId, Long productId, Long optionId) {
 		Cart cart = cartComponent.findCart(memberId);
 		
-		Product product = productRepository.findById(request.productId())
+		Product product = productRepository.findById(productId)
 				.orElseThrow(() -> new CartHandler(ErrorStatus.PRODUCT_NOT_FOUND));
 		
 		ProductOption option;
-		if (request.optionId() != null) {
-			option = productOptionRepository.findById(request.optionId())
-					.filter(opt -> opt.getProduct().getId().equals(product.getId()))
+		if (optionId != null) {
+			option = productOptionRepository.findById(optionId)
+					.filter(opt -> opt.getProduct().getId().equals(productId))
 					.orElseThrow(() -> new CartHandler(ErrorStatus.OPTION_NOT_FOUND));
 		} else {
 			option = null;
 		}
 		
-		Optional<CartProduct> existingItem = cartProductRepository.findByCartIdAndProductId(cart.getId(), product.getId());
-		
-		existingItem.ifPresentOrElse(
-				item -> {
-					log.info("[CartCommandService] 기존 아이템 {}의 옵션과 수량을 변경합니다.", item.getId());
-					item.update(option, request.quantity());
-				},
-				() -> {
-					log.info("[CartCommandService] 장바구니에 새로 아이템 {}를 추가합니다.", request.productId());
-					CartProduct newCartProduct = CartProduct.create(cart, product, option, request.quantity());
-					cart.addCartProduct(newCartProduct);
-					cartProductRepository.save( newCartProduct);
-				}
-		);
+		return new CartOpterationContext(cart, product, option);
 	}
+	
+	private record CartOpterationContext(Cart cart, Product product, ProductOption productOption) {}
 }
