e693ed3260c0e17d0daa10ded959ad80ce08f5e2
SONG <dlthdal07@gmail.com>
2025-07-18 17:47:27 +0900

    :recycle: refactor: SecurityConfig 카테고리 경로 추가

--- src/main/java/com/jajaja/global/config/security/SecurityConfig.java
+++ src/main/java/com/jajaja/global/config/security/SecurityConfig.java
@@ -27,7 +27,8 @@ public class SecurityConfig {
             "/swagger-ui/**",
             "/v3/api-docs/**",
             "/api/auth/token",
-            "/api/products/**"
+            "/api/products/**",
+            "/api/categories/**",
     };
 
     @Bean
