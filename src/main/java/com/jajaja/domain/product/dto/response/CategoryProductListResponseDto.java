f207b10ac9da90fd464d7994a5889afcb6b177da
SONG <dlthdal07@gmail.com>
2025-07-19 23:29:57 +0900

    :recycle: refactor: 하위 카테고리 상품 목록 조회 응답 dto PageResponse 기반으로 통일

--- src/main/java/com/jajaja/domain/product/dto/response/CategoryProductListResponseDto.java
+++ src/main/java/com/jajaja/domain/product/dto/response/CategoryProductListResponseDto.java
@@ -1,16 +1,14 @@
 package com.jajaja.domain.product.dto.response;
 
+import com.jajaja.global.apiPayload.PageResponse;
 import lombok.Builder;
 
 import java.util.List;
 
 public record CategoryProductListResponseDto(
-        List<ProductItemDto> content,
-        int page,
-        int size,
-        boolean hasNext
+        PageResponse page,
+        List<ProductItemDto> products
 ) {
-
     @Builder
     public record ProductItemDto(
             Long productId,
