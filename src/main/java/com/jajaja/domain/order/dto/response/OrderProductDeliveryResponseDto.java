93414aed863c339db73733f70e927db22c2cbed1
Jinsoo Yoon <floreo1242@gmail.com>
2025-07-20 20:37:21 +0900

    ✨ feat: 응답에 택배사 및 배송 정보 필드 추가

--- src/main/java/com/jajaja/domain/order/dto/response/OrderProductDeliveryResponseDto.java
+++ src/main/java/com/jajaja/domain/order/dto/response/OrderProductDeliveryResponseDto.java
@@ -1,11 +1,19 @@
 package com.jajaja.domain.order.dto.response;
 
 import com.jajaja.domain.order.entity.OrderProduct;
+import lombok.Builder;
 
+@Builder
 public record OrderProductDeliveryResponseDto(
-        String invoiceNumber
+        String invoiceNumber,
+        String courier,
+        OrderDeliveryDto delivery
 ) {
     public static OrderProductDeliveryResponseDto from(OrderProduct orderProduct) {
-        return new OrderProductDeliveryResponseDto(orderProduct.getInvoiceNumber());
+        return OrderProductDeliveryResponseDto.builder()
+                .invoiceNumber(orderProduct.getInvoiceNumber())
+                .courier("CJ대한통운")
+                .delivery(OrderDeliveryDto.from(orderProduct.getOrder().getDelivery()))
+                .build();
     }
 }
