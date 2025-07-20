23f0917711c6b689d9abd47911bff267ff00f4e8
YYJ <yoon20220823@dukcung.ac.kr>
2025-07-20 15:34:29 +0900

    🎨 Style: SearchController @GetMapping 및 @Operation 개선
     SearchController @GetMapping 및 @Operation 개선

--- src/main/java/com/jajaja/domain/search/controller/SearchController.java
+++ src/main/java/com/jajaja/domain/search/controller/SearchController.java
@@ -18,8 +18,10 @@ public class SearchController {
 
     private final SearchService searchService;
 
-    @Operation(summary = "상품 검색 API | by 구름/윤윤지", description = "키워드 기반으로 상품을 검색하고, 정렬 기준에 따라 결과를 반환합니다.")
-    @GetMapping("")
+    @Operation(summary = "상품 검색 API | by 구름/윤윤지",
+            description = "키워드 기반으로 상품을 검색하고, 정렬 기준에 따라 결과를 반환합니다."
+    )
+    @GetMapping
     public ApiResponse<List<ProductListResponseDto>> searchProducts(
             @RequestParam String keyword,
             @RequestParam(defaultValue = "POPULAR") SearchSort sort) {
@@ -28,7 +30,9 @@ public class SearchController {
         return ApiResponse.onSuccess(result);
     }
 
-    @Operation(summary = "인기 검색어 조회 API | by 구름/윤윤지", description = "최근 인기 검색어 8개와 기준 시간을 반환합니다.")
+    @Operation(summary = "인기 검색어 조회 API | by 구름/윤윤지",
+            description = "최근 인기 검색어 8개와 기준 시간을 반환합니다."
+    )
     @GetMapping("/popular-keywords")
     public ApiResponse<PopularSearchKeywordsResponseDto> getPopularKeywords() {
         PopularSearchKeywordsResponseDto popularKeywords = searchService.getPopularKeywords();
