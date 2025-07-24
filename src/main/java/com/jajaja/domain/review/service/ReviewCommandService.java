d31fd1d7892c2349a7863eea86707c34fe8da625
SONG <dlthdal07@gmail.com>
2025-07-24 14:18:41 +0900

    :sparkles: feat: 리뷰 추가 API 구현

---
+++ src/main/java/com/jajaja/domain/review/service/ReviewCommandService.java
@@ -0,0 +1,7 @@
+package com.jajaja.domain.review.service;
+
+import com.jajaja.domain.review.dto.request.ReviewCreateRequestDto;
+
+public interface ReviewCommandService {
+    Long createReview(Long memberId, Long productId, ReviewCreateRequestDto requestDto);
+}
