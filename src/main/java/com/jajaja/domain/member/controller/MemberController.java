7a0e7f146467ba58bb3d563eaf231ab9643bbb2b
Jinsoo Yoon <floreo1242@gmail.com>
2025-07-27 21:04:02 +0900

    ✨ feat: Swagger 문서화 추가

--- src/main/java/com/jajaja/domain/member/controller/MemberController.java
+++ src/main/java/com/jajaja/domain/member/controller/MemberController.java
@@ -6,6 +6,7 @@ import com.jajaja.domain.member.service.MemberCommandService;
 import com.jajaja.domain.member.service.MemberQueryService;
 import com.jajaja.global.apiPayload.ApiResponse;
 import com.jajaja.global.config.security.annotation.Auth;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,12 +21,21 @@ public class MemberController {
     private final MemberQueryService memberQueryService;
     private final MemberCommandService memberCommandService;
 
+    @Operation(
+            summary = "내 정보 조회",
+            description = "로그인한 사용자의 정보를 조회합니다."
+    )
     @GetMapping("/me")
     public ApiResponse<MemberInfoResponseDto> getMyInfo(@Auth Long memberId) {
         MemberInfoResponseDto memberInfoResponseDto = memberQueryService.getMemberInfo(memberId);
         return ApiResponse.onSuccess(memberInfoResponseDto);
     }
 
+    @Operation(
+            summary = "회원 정보 수정",
+            description = "memberId 사용자의 정보를 수정합니다. \n" +
+                    "수정이 필요하지 않은 필드는 null(혹은 미포함)로 요청하면 됩니다."
+    )
     @PatchMapping("/{memberId}")
     public ApiResponse<MemberInfoResponseDto> updateMemberInfo(@PathVariable Long memberId,
                                                                @RequestBody @Valid MemberProfileUpdateRequest request) {
