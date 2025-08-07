5cf337acf8b6676f22646cd6852bbf7b4e415640
Jinsoo Yoon <floreo1242@gmail.com>
2025-08-07 14:07:40 +0900

    🐛 fix: 데이터 반환 시 저장된 keyName으로부터 URL을 생성하도록 수정

--- src/main/java/com/jajaja/domain/team/dto/response/TeamProductItemResponseDto.java
+++ src/main/java/com/jajaja/domain/team/dto/response/TeamProductItemResponseDto.java
@@ -1,6 +1,5 @@
 package com.jajaja.domain.team.dto.response;
 
-import com.jajaja.domain.member.entity.Member;
 import com.jajaja.domain.product.entity.Product;
 import com.jajaja.domain.team.entity.Team;
 import lombok.Builder;
@@ -16,14 +15,13 @@ public record TeamProductItemResponseDto(
         Integer discountRate,
         String thumbnailUrl
 ) {
-    public static TeamProductItemResponseDto from(Team team) {
+    public static TeamProductItemResponseDto of(Team team, String profileUrl) {
         Product product = team.getProduct();
-        Member leader = team.getLeader();
 
         return TeamProductItemResponseDto.builder()
                 .teamId(team.getId())
                 .nickname(team.getLeader().getName())
-                .leaderProfileImageUrl(leader.getProfileUrl())
+                .leaderProfileImageUrl(profileUrl)
                 .productId(product.getId())
                 .productName(product.getName())
                 .price(product.getPrice())
