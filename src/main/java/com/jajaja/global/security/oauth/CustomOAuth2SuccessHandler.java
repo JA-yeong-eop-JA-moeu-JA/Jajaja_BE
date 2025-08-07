fc9b1024f51319de65031845d9e738fdaab9c339
Jinsoo Yoon <floreo1242@gmail.com>
2025-08-07 17:14:53 +0900

    ✨ feat: 로그인 성공 시 리프레시 토큰을 Redis에 저장

--- src/main/java/com/jajaja/global/security/oauth/CustomOAuth2SuccessHandler.java
+++ src/main/java/com/jajaja/global/security/oauth/CustomOAuth2SuccessHandler.java
@@ -1,5 +1,7 @@
 package com.jajaja.global.security.oauth;
 
+import com.jajaja.domain.redis.entity.RefreshToken;
+import com.jajaja.domain.redis.repository.RefreshTokenRepository;
 import com.jajaja.global.security.jwt.JwtProperties;
 import com.jajaja.global.security.jwt.JwtProvider;
 import jakarta.servlet.ServletException;
@@ -18,11 +20,14 @@ public class CustomOAuth2SuccessHandler implements AuthenticationSuccessHandler 
 
     private final JwtProvider jwtProvider;
     private final JwtProperties jwtProperties;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         String accessToken = jwtProvider.generateAccessToken(authentication);
         String refreshToken = jwtProvider.generateRefreshToken(authentication);
+        RefreshToken refreshTokenEntity = new RefreshToken(authentication.getName(), refreshToken);
+        refreshTokenRepository.save(refreshTokenEntity);
         jwtProvider.writeTokenCookies(response, accessToken, refreshToken);
         response.sendRedirect(jwtProperties.getRedirectUrl());
     }
