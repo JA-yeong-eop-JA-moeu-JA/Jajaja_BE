cc25a7931377ddad06d615e3265b81044ce83ba3
Jinsoo Yoon <floreo1242@gmail.com>
2025-08-07 17:06:23 +0900

    🐛 fix: RefreshToken 엔티티에서 data.annotation.Id 어노테이션을 사용하도록 수정

--- src/main/java/com/jajaja/domain/redis/entity/RefreshToken.java
+++ src/main/java/com/jajaja/domain/redis/entity/RefreshToken.java
@@ -1,9 +1,9 @@
 package com.jajaja.domain.redis.entity;
 
-import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @Getter
