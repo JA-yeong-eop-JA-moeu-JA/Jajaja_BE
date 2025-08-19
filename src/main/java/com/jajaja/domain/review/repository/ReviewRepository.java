0d1bbb6b9ca622f8a766725625e15c919c117d4f
SONG <dlthdal07@gmail.com>
2025-08-19 22:51:58 +0900

    :recycle: Refactor: 리뷰 작성 API 로직 개선

--- src/main/java/com/jajaja/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/jajaja/domain/review/repository/ReviewRepository.java
@@ -1,9 +1,11 @@
 package com.jajaja.domain.review.repository;
 
+import com.jajaja.domain.order.entity.OrderProduct;
 import com.jajaja.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
+    boolean existsByOrderProduct(OrderProduct orderProduct);
 }
